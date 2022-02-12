a69bf40fa51cb808559dcfbab55fcf816401ed59
cryptovinci <0xcryptovinci@protonmail.com>
2022-02-12 02:13:11 -0500

    first commit

---
+++ src/Bananas.jsx
@@ -0,0 +1,67 @@
+import { Suspense, useRef, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import * as THREE from "three";
+import { useGLTF, Environment } from "@react-three/drei";
+import { EffectComposer, DepthOfField } from "@react-three/postprocessing";
+
+function Banana({ z }) {
+  const ref = useRef();
+  const { nodes, materials } = useGLTF("/banana-transformed.glb");
+  const { viewport, camera } = useThree();
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
+  const [data] = useState({
+    x: THREE.MathUtils.randFloatSpread(2),
+    y: THREE.MathUtils.randFloatSpread(height),
+    rX: Math.random() * Math.PI,
+    rY: Math.random() * Math.PI,
+    rZ: Math.random() * Math.PI,
+  });
+
+  useFrame((state) => {
+    ref.current.rotation.set(
+      (data.rX += 0.001),
+      (data.rY += 0.001),
+      (data.rZ += 0.005)
+    );
+    ref.current.position.set(width * data.x, (data.y += 0.025), z);
+    if (data.y > height) {
+      data.y = -height;
+    }
+  });
+  return (
+    <mesh
+      ref={ref}
+      geometry={nodes.banana.geometry}
+      material={materials.skin}
+      material-emissive="orange"
+    ></mesh>
+  );
+}
+
+export default function Bananas({ count = 100, depth = 80 }) {
+  return (
+    <Canvas gl={{ alpha: false }} camera={{ near: 0.01, far: 110, fov: 30 }}>
+      <color attach="background" args={["#ffbf40"]} />
+      <spotLight
+        position={[10, 10, 10]}
+        penumbra={1}
+        intensity={3}
+        color="orange"
+      />
+      <Suspense fallback={null}>
+        <Environment preset="sunset" />
+        {Array.from({ length: count }, (_, i) => (
+          <Banana key={i} z={-(i / count) * depth - 20} />
+        ))}
+        <EffectComposer>
+          <DepthOfField
+            target={[0, 0, depth / 2]}
+            focalLength={0.5}
+            bokehScale={1}
+            height={700}
+          />
+        </EffectComposer>
+      </Suspense>
+    </Canvas>
+  );
+}
